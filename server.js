7f76f62a09600da56d9e6a88cebd1949b7113b61
OliverTwist <phantom8@inbox.ru>
2026-04-28 13:18:05 +0500

    first commit

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+app.use(cors());
+
+app.get("/", (req, res) => {
+  res.send("Сервер работает 🚀");
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log("Сервер запущен");
+});
