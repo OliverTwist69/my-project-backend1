28f08ea2f21db9ef51b3034f9cd75a76bc029443
OliverTwist <phantom8@inbox.ru>
2026-05-03 20:18:27 +0500

    update text

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const app = express();
 app.use(cors());
 
 app.get("/", (req, res) => {
-  res.send("Сервер работает 🚀2322");
+  res.send("Сервер работает 🚀цццйцуфывфы2322");
 });
 
 const PORT = process.env.PORT || 3000;
