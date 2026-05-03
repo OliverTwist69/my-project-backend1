75e9ff1f23b8da63346db1b7c218c7eec7abc1f1
OliverTwist <phantom8@inbox.ru>
2026-05-03 15:25:59 +0500

    added jobs

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const app = express();
 app.use(cors());
 
 app.get("/", (req, res) => {
-  res.send("Сервер работает 🚀");
+  res.send("Сервер работает 🚀2322");
 });
 
 const PORT = process.env.PORT || 3000;
